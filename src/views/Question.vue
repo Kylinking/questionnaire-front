8a82489cfb9140a4d91025fd97020ce0fe1b6218
kylin-Mac <kylinsmail@gmail.com>
2019-08-18 00:26:55 +0800

    界面修改
    增加rating为1时的文本输入栏
    增加每个问题的答案的内存记录，在back和next时为填入已选择过的内容

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -4,28 +4,41 @@
 
     <v-card class="mx-auto" max-width="500">
       <v-card-title class="title font-weight-regular justify-space-between">
-        <span>{{ questions[step].Title }}</span>
+        <span class="display-1">{{ questions[step].Index }}</span>
       </v-card-title>
 
       <v-window v-model="step">
-        <v-window-item v-for="(item,index) in questions" :key="index">
-          <v-card-text>{{ item.Content }}</v-card-text>
+        <v-window-item v-for="item in questions" :key="item.Id">
+          <v-card-text class="headline">{{ item.Content }}</v-card-text>
         </v-window-item>
-        <v-card-text>
-          <div class="text-center">
-            <v-rating v-model="rating" empty-icon="mdi-thumb-up-outline" full-icon="mdi-thumb-up"></v-rating>
-          </div>
-        </v-card-text>
       </v-window>
-
+      <v-card-text>
+        <v-row cols="12">
+          <v-col xs="1" class="text-center" align-self="center">
+            <span class="subtitle-2">不满意</span>
+          </v-col>
+          <v-col xs="10" class="text-center" align-self="start">
+            <v-rating v-model="rating" empty-icon="mdi-thumb-up-outline" full-icon="mdi-thumb-up"></v-rating>
+          </v-col>
+          <v-col xs="1" class="text-center" align-self="center">
+            <span align="center" class="subtitle-2">非常满意</span>
+          </v-col>
+        </v-row>
+        <v-card class="mx-auto" max-width="500" v-if="rating == 1">
+          <v-card-title class="subtitle-2">不满意的原因以及意见或建议</v-card-title>
+          <v-col cols="12">
+            <v-textarea required outlined name="input-7-4" label="不满意原因、意见或建议" v-model="reason"></v-textarea>
+          </v-col>
+        </v-card>
+      </v-card-text>
       <v-card-actions>
-        <v-btn :disabled="step === 0" text @click="step--">Back</v-btn>
+        <v-btn :disabled="step === 0" text @click="back()">Back</v-btn>
         <v-spacer></v-spacer>
         <v-btn
           :disabled="step === questions.length-1"
           color="primary"
           depressed
-          @click="step++"
+          @click="next()"
         >Next</v-btn>
       </v-card-actions>
     </v-card>
@@ -33,27 +46,75 @@
 </template>
 <script>
 export default {
+  methods: {
+    next: function() {
+      this.result[this.step] = {
+        questionId: this.step,
+        rating: this.rating,
+        reason: this.reason
+      };
+      this.step++;
+      if (!this.result[this.step]) {
+        this.rating = 3;
+        this.reason = "";
+      } else {
+        this.rating = this.result[this.step].rating;
+        this.reason = this.result[this.step].reason;
+      }
+    },
+    back: function() {
+      this.step--;
+      if (!this.result[this.step]) {
+        this.rating = 3;
+        this.reason = "";
+      } else {
+        this.rating = this.result[this.step].rating;
+        this.reason = this.result[this.step].reason;
+      }
+    }
+  },
   data() {
     return {
       currentTitle: "后勤服务",
       step: 0,
       rating: 3,
+      result: [],
       questions: [
         {
-          Title: "标题一",
-          Content: "这是描述1，这是描述1，这是描述1"
+          Id: 1,
+          Index: "课堂育人",
+          Content: "课堂教育教学的育人活动",
+          Department: null,
+          CreatedAt: "2019-08-16 09:37:53",
+          UpdatedAt: "2019-08-16 09:37:53",
+          QuestionnaireId: 1
         },
         {
-          Title: "标题二",
-          Content: "这是描述2，这是描述2，这是描述2"
+          Id: 2,
+          Index: "课外育人",
+          Content: "学校开展的课堂教育教学以外的育人活动",
+          Department: null,
+          CreatedAt: "2019-08-16 09:37:53",
+          UpdatedAt: "2019-08-16 09:37:53",
+          QuestionnaireId: 1
         },
         {
-          Title: "标题三",
-          Content: "这是描述3，这是描述3，这是描述3"
+          Id: 3,
+          Index: "思想政治课",
+          Content: "学校开设的思想政治理论课程的教材、教师、教学等方面",
+          Department: null,
+          CreatedAt: "2019-08-16 09:37:53",
+          UpdatedAt: "2019-08-16 09:37:53",
+          QuestionnaireId: 1
         },
         {
-          Title: "标题四",
-          Content: "这是描述4，这是描述4，这是描述4"
+          Id: 4,
+          Index: "公共基础课",
+          Content: "不含思想政治课的其它公共课程",
+          Department: null,
+          CreatedAt: "2019-08-16 09:37:53",
+          UpdatedAt: "2019-08-16 09:37:53",
+          QuestionnaireId: 1
         }
       ]
     };
